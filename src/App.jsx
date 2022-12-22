ac4b1ec319f13678e7edc3d8e8499e5ae92565d4
JonasAX <jonasxavier@tutamail.com>
2022-12-22 10:04:33 -0500

    play/pause behaviour

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,27 @@
-import React from "react";
 import "./App.css";
 import Planet from "./components/planet/planet";
-import { useState } from "react";
+import { useEffect } from "react";
+
 
 export default function App() {
-    const [planetSound, setPlanetSound] = useState();
+  // Only one audio must be played. Therefore, it is placed outside of <Planet/>
+  let audio = new Audio();
 
-    return (
-        <div className="app">
-            <h1>Solar System Sounds</h1>
-            <div className="planetContainer">
-                <Planet
-                    planetName="sun"
-                    planetSound={planetSound}
-                    setPlanetSound={setPlanetSound}
-                />
-                <Planet
-                    planetName="mercury"
-                    planetSound={planetSound}
-                    setPlanetSound={setPlanetSound}
-                />
-                <Planet
-                    planetName="venus"
-                    planetSound={planetSound}
-                    setPlanetSound={setPlanetSound}
-                />
-                <Planet
-                    planetName="mars"
-                    planetSound={planetSound}
-                    setPlanetSound={setPlanetSound}
-                />
-                <Planet
-                    planetName="earth"
-                    planetSound={planetSound}
-                    setPlanetSound={setPlanetSound}
-                />
-                <Planet
-                    planetName="jupiter"
-                    planetSound={planetSound}
-                    setPlanetSound={setPlanetSound}
-                />
-                <Planet
-                    planetName="saturn"
-                    planetSound={planetSound}
-                    setPlanetSound={setPlanetSound}
-                />
-                <Planet
-                    planetName="uranus"
-                    planetSound={planetSound}
-                    setPlanetSound={setPlanetSound}
-                />
-                <Planet
-                    planetName="neptune"
-                    planetSound={planetSound}
-                    setPlanetSound={setPlanetSound}
-                />
-                <Planet
-                    planetName="pluto"
-                    planetSound={planetSound}
-                    setPlanetSound={setPlanetSound}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className="app">
+      <h1>Solar System Sounds</h1>
+      <div className="planetContainer">
+        <Planet planetName="sun" audio={audio} />
+        <Planet planetName="mercury" audio={audio} />
+        <Planet planetName="venus" audio={audio} />
+        <Planet planetName="mars" audio={audio} />
+        <Planet planetName="earth" audio={audio} />
+        <Planet planetName="jupiter" audio={audio} />
+        <Planet planetName="saturn" audio={audio} />
+        <Planet planetName="uranus" audio={audio} />
+        <Planet planetName="neptune" audio={audio} />
+        <Planet planetName="pluto" audio={audio} />
+      </div>
+    </div>
+  );
 }
--- src/components/planet/planet.jsx
+++ src/components/planet/planet.jsx
@@ -1,111 +1,105 @@
 import "./planet.css";
-import { useEffect } from "react";
 // images
-import sunImage from "../../assets/sun.jpg"
-import mercuryImage from "../../assets/mercury.jpg"
-import venusImage from "../../assets/venus.jpg"
-import earthImage from "../../assets/earth.webp"
-import marsImage from "../../assets/mars.png"
-import jupiterImage from "../../assets/jupiter.png"
-import saturnImage from "../../assets/saturn.gif"
-import uranusImage from "../../assets/uranus.png"
-import neptuneImage from "../../assets/neptune.jpg"
-import plutoImage from "../../assets/pluto.jpg"
+import sunImage from "../../assets/sun.jpg";
+import mercuryImage from "../../assets/mercury.jpg";
+import venusImage from "../../assets/venus.jpg";
+import earthImage from "../../assets/earth.webp";
+import marsImage from "../../assets/mars.png";
+import jupiterImage from "../../assets/jupiter.png";
+import saturnImage from "../../assets/saturn.gif";
+import uranusImage from "../../assets/uranus.png";
+import neptuneImage from "../../assets/neptune.jpg";
+import plutoImage from "../../assets/pluto.jpg";
 // sounds
-import sunSound from "../../assets/sun.mp3"
-import mercurySound from "../../assets/mercury.mp3"
-import venusSound from "../../assets/venus.mp3"
-import earthSound from "../../assets/earth.mp3"
-import marsSound from "../../assets/mars.mp3"
-import jupiterSound from "../../assets/jupiter.mp3"
-import saturnSound from "../../assets/saturn.mp3"
-import uranusSound from "../../assets/uranus.mp3"
-import neptuneSound from "../../assets/neptune.mp3"
-import plutoSound from "../../assets/pluto.mp3"
+import sunSound from "../../assets/sun.mp3";
+import mercurySound from "../../assets/mercury.mp3";
+import venusSound from "../../assets/venus.mp3";
+import earthSound from "../../assets/earth.mp3";
+import marsSound from "../../assets/mars.mp3";
+import jupiterSound from "../../assets/jupiter.mp3";
+import saturnSound from "../../assets/saturn.mp3";
+import uranusSound from "../../assets/uranus.mp3";
+import neptuneSound from "../../assets/neptune.mp3";
+import plutoSound from "../../assets/pluto.mp3";
 
-function Planet({ planetName, planetSound, setPlanetSound }) {
-    // Play the sound inserted in planetName
-    useEffect(() => {
-        // By default, every useEffect runs on mount. This should not happen here
-        if (planetSound) {
-            planetSound.play();
-        }
-    }, [planetSound]);
 
-    const planets = {
-        sun: {
-            title: "sun",
-            image: sunImage,
-            sound: sunSound,
-        },
-        mercury: {
-            title: "mercury",
-            image: mercuryImage,
-            sound: mercurySound,
-        },
-        venus: {
-            title: "venus",
-            image: venusImage,
-            sound: venusSound,
-        },
-        mars: {
-            title: "mars",
-            image: marsImage,
-            sound: marsSound,
-        },
-        earth: {
-            title: "earth",
-            image: earthImage,
-            sound: earthSound,
-        },
-        jupiter: {
-            title: "jupiter",
-            image: jupiterImage,
-            sound: jupiterSound,
-        },
-        saturn: {
-            title: "saturn",
-            image: saturnImage,
-            sound: saturnSound,
-        },
-        uranus: {
-            title: "uranus",
-            image: uranusImage,
-            sound: uranusSound,
-        },
-        neptune: {
-            title: "neptune",
-            image: neptuneImage,
-            sound: neptuneSound,
-        },
-        pluto: {
-            title: "pluto",
-            image: plutoImage,
-            sound: plutoSound,
-        },
-    };
+const planets = {
+  sun: {
+    title: "sun",
+    image: sunImage,
+    sound: sunSound,
+  },
+  mercury: {
+    title: "mercury",
+    image: mercuryImage,
+    sound: mercurySound,
+  },
+  venus: {
+    title: "venus",
+    image: venusImage,
+    sound: venusSound,
+  },
+  mars: {
+    title: "mars",
+    image: marsImage,
+    sound: marsSound,
+  },
+  earth: {
+    title: "earth",
+    image: earthImage,
+    sound: earthSound,
+  },
+  jupiter: {
+    title: "jupiter",
+    image: jupiterImage,
+    sound: jupiterSound,
+  },
+  saturn: {
+    title: "saturn",
+    image: saturnImage,
+    sound: saturnSound,
+  },
+  uranus: {
+    title: "uranus",
+    image: uranusImage,
+    sound: uranusSound,
+  },
+  neptune: {
+    title: "neptune",
+    image: neptuneImage,
+    sound: neptuneSound,
+  },
+  pluto: {
+    title: "pluto",
+    image: plutoImage,
+    sound: plutoSound,
+  },
+};
 
-    const playPlanetSound = () => {
-        // Stop the current audio, if it exist.
-        if (planetSound) {
-            planetSound.load();
-        }
+function Planet({ planetName, audio }) {
+  const planetClicked = () => {
 
-        // Bring the audio file to useState, so the useEffect will play it.
-        // The audio file is stored outside this component in order to make it stoppable by another component.
-        setPlanetSound(new Audio(planets[planetName]["sound"]));
-    };
+    // this "if" will add a "play/pause" behaviour, instead of just "play" behaviour
+    if (audio?.getAttribute("src") === planets[planetName]["sound"] && !audio.paused) {
+      audio.load();
+      return;
+    }
 
-    return (
-        <div className="planet grow" onClick={() => playPlanetSound()} >
-            <p className="planetTitle">{planets[planetName]["title"]}</p>
-            <img
-                className="picture"
-                src={planets[planetName]["image"]}
-                alt={planets[planetName]["title"]}
-            />
-        </div>
-    );
+    audio.load();
+    audio = new Audio(planets[planetName]["sound"]);
+    audio.play();
+  };
+
+  return (
+    <div className="planet grow" onClick={() => planetClicked()}>
+      <p className="planetTitle">{planets[planetName]["title"]}</p>
+      <img
+        className="picture"
+        src={planets[planetName]["image"]}
+        alt={planets[planetName]["title"]}
+      />
+    </div>
+  );
 }
 
 export default Planet;
